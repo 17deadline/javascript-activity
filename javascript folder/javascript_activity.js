8630c5f301dc3a0abbaa271adf0d7f55c52b1153
Gabriel Hwang <ghfr06@gmail.com>
2025-02-11 13:37:57 -0500

    Feb 11 2025 progress

--- javascript folder/javascript_activity.js
+++ javascript folder/javascript_activity.js
@@ -1,9 +1,3 @@
-function sayHello() {
-    alert("Hello, JavaScript is working!");
-}
-
-console.log("JavaScript file is linked!");
-
 
 // Part 1: Variables and Data Types
 
@@ -21,21 +15,21 @@ console.log("Is student? " + isStudent);
 
 // Defining a function with parameters
 function greet(name) {
-var greeting = "Hello, " + name + "!";
-console.log(greeting);
+    var greeting = "Hello, " + name + "!";
+    console.log(greeting);
 }
 
 // Calling the function with arguments
-greet("Alice");
+greet("Gabe");  
 greet("Bob");
 
 // Accessing a variable inside and outside a function
 var globalVar = "I am global";
 
 function printVars() {
-var localVar = "I am local";
-console.log(localVar);
-console.log(globalVar);
+    var localVar = "I am local";
+    console.log(localVar);
+    console.log(globalVar);
 }
 
 printVars();
@@ -46,18 +40,18 @@ printVars();
 var numbers = [1, 2, 3, 4, 5];
 
 for (var i = 0; i < numbers.length; i++) {
-console.log(numbers[i]);
+    console.log(numbers[i]);
 }
 
 // Part 4: Objects and Methods
 
 // Creating an object with a method
 var person = {
-name: "John",
-age: 30,
-sayHello: function() {
-console.log("Hello, my name is " + this.name);
-}
+    name: "John",
+    age: 30,
+    sayHello: function() {
+        console.log("Hello, my name is " + this.name);
+    }
 };
 
 // Calling the method of an object
@@ -67,12 +61,19 @@ person.sayHello();
 
 // Getting an element by ID and modifying its content
 var heading = document.getElementById("heading");
-heading.innerHTML = "programming with JavaScript";
+heading.innerHTML = "TEST TEST TEST TEST";
 
 // Creating a new element and appending it to the document
 var paragraph = document.createElement("p");
 paragraph.innerHTML = "This is a new paragraph.";
 document.body.appendChild(paragraph);
+
+//me testing out what creating a new element does
+var heading2 = document.createElement("h2");
+heading2.innerHTML = "test of heading 2";
+console.log(heading2);
+document.body.appendChild(heading2);
+
 
 function displayVariables() {
 var output = document.getElementById('output');
